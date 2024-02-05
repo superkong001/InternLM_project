eb44af23aaa8b9f433bce0d94bacc30f7c2b9e64
telos <37318654+superkong001@users.noreply.github.com>
2024-02-05 20:45:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == '__main__':
     # os.system('streamlit run react_Oculi_web_demo.py --server.address 0.0.0.0 --server.port 7860 --server.enableStaticServing True')
-    os.system('streamlit run react_Oculi_web.py --server.address 0.0.0.0 --server.port 7860 --server.enableStaticServing True')
+    os.system('streamlit run react_Oculi_cli.py')
