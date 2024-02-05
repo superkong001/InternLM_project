707743f5f60bcaa1e3ab853ba7ba3383f4fcafd9
telos <37318654+superkong001@users.noreply.github.com>
2024-02-05 23:03:08 +0800

    Update fundus_diagnosis.py

--- fundus_diagnosis.py
+++ fundus_diagnosis.py
@@ -24,7 +24,8 @@ class FundusDiagnosis(BaseAction):
                  name: Optional[str] = None,
                  enable: bool = True,
                  disable_description: Optional[str] = None) -> None:
-        super().__init__(description, name, enable, disable_description)
+        # super().__init__(description, name, enable, disable_description)
+        super().__init__(description)
 
         if model_path is not None:
             assert os.path.exists(model_path), f"model_path: {model_path} not exists"
